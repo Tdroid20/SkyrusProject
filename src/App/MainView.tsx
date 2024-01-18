e75563de9a19b30e6726ce1612f3dd700ea27093
tdroid <tdroid@skyrushub.xyz>
2024-01-18 01:46:03 -0300

    Feat: Glitch Effect Implemented

--- src/App/MainView.tsx
+++ src/App/MainView.tsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import skyrushub from '../assets/SkyrusHubFullWhite.png';
 import './Styled.mainView.css'
 
 export const MainView: React.FC = () => {
+    const [glitchButtons, setGlitchButtons] = useState(false);
+    const [glitchFull, setGlitchFull] = useState(false);
+    const [isStarted, updateExp] = useState(false);
+    const [showMessage, updateShowMessage] = useState(false)
+    const [cursorLocked, setCursorLocked] = useState(false);
+    
+    function initExp() {
+        setTimeout(() => {
+            setGlitchFull(true)
+        }, 2000);
+
+        setTimeout(() => {
+            updateExp(true);
+            updateShowMessage(true);
+        }, 2666.67);
+    }
+
+    function hanndleClickrReady() {
+        setGlitchButtons(true);
+        setCursorLocked(true);
+        initExp();
+        setTimeout(() => {
+            updateExp(true);
+            setCursorLocked(false);
+            setGlitchButtons(false);
+            setGlitchFull(false);
+            updateShowMessage(false);
+        }, 8000);
+    }
+
+    useEffect(() => {
+        if (cursorLocked) {
+          document.body.classList.add('cursor-locked');
+        } else {
+          document.body.classList.remove('cursor-locked');
+        }
+      }, [cursorLocked]);
     return (
-        <div className="MainApp">
+        <div className={`glass ${glitchFull ? "glassGlitch" : ""}`}>
+            <div className={`MainApp`}>
             <header className="Header">
                 <div className="hContainer">
                     <img src={skyrushub} width={"370px"} onDragStart={(e) => e.preventDefault()} />
@@ -12,12 +50,13 @@ export const MainView: React.FC = () => {
                 </div>
             </header>
             <div className="content">
-                <h1 className="typewriter-text">Coming Soon...</h1>
+                <h1 className="typewriter-text">{showMessage ? "Coming Soon..." : "SEJA BEM VINDO A REAL EXPERIÊNCIA"}</h1>
                 <div className="bottomM">
-                    <p className="readyButton">Ready?</p>
+                    <p className={`readyButton ${glitchButtons ? "glitch" : ""} ${cursorLocked ? "cursor-locked" : ""}`} onClick={() => hanndleClickrReady()}>Ready?</p>
                 </div>
                       <p className="copyright">Copyright © 2023 - 2024, SkyrusHub LTDA. Todos os direitos reservados.</p>
             </div>
+        </div>
         </div>
     )
 }
